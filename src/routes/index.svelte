26f760901821710f7db4de89c3d1a8f1292406fc
trish <caburian@greenova.de>
2021-12-01 21:39:29 +0800

    working supabase connection

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!--<h1>Welcome to SvelteKit</h1>-->
+<!--<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>-->
+<script>
+   import supabase from "$lib/db";
+
+   async function getData() {
+       const { data, error } = await supabase
+           .from('countries')
+           .select()
+       if (error) throw new Error(error.message)
+
+       return data
+   }
+</script>
+
+{#await getData()}
+    <p>Fetching data</p>
+{:then data}
+    {#each data as country}
+        <li>{country.name}</li>
+    {/each}
+{:catch error}
+    <p>Error</p>
+    <pre>{error}</pre>
+{/await}
