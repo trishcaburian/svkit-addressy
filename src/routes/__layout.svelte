db76905af168f7d4a9c47bb38b1496365148f401
trish <caburian@greenova.de>
2021-12-01 22:45:13 +0800

    Added site header + placeholder pages

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,8 @@
+<nav class="flex items-center justify-between flex-wrap bg-gray-800 p-6 text-white">
+    <span class="font-bold text-xl">Address Book</span>
+    <div class="w-full block flex-grow px-4 sm:flex sm:items-center sm:w-auto">
+        <div class="sidebar" on:click={() => (window.location.href = "/")}> Home </div>
+        <div class="sidebar" on:click={() => (window.location.href = "/add_contact")}> Add </div>
+    </div>
+</nav>
+<slot></slot>
---
+++ src/routes/extra/bonus.svelte
@@ -0,0 +1,1 @@
+congrats you found this page!
