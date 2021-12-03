24dc5e5ec1ba12695a15424a0944f9d2fd5eb86e
trish <caburian@greenova.de>
2021-12-03 21:38:17 +0800

    Home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,23 +3,50 @@
 <script>
    import supabase from "$lib/db";
 
+   let search_text;
+
    async function getData() {
        const { data, error } = await supabase
-           .from('countries')
+           .from('contact')
            .select()
        if (error) throw new Error(error.message)
 
        return data
    }
 </script>
-
-{#await getData()}
-    <p>Fetching data</p>
-{:then data}
-    {#each data as country}
-        <li>{country.name}</li>
-    {/each}
-{:catch error}
-    <p>Error</p>
-    <pre>{error}</pre>
-{/await}
+<div class="p-5 flex justify-center flex-grow">
+    <div class="md:w-3/4 bg-white">
+        <div class="w-full sticky top-0 bg-white place-items-center">
+            <div class="p-5">
+                <input type="text" class="regular_field w-full" bind:value={search_text} placeholder="Search"/>
+            </div>
+        </div>
+        <div class="mt-1">
+            {#await getData()}
+                <p>Fetching data</p>
+            {:then data}
+                {#each data as contact}
+                    <hr class="w-full">
+                    <div class="w-full block flex-grow px-4 sm:flex sm:items-center sm:w-auto" on:click={() => {
+                        console.log('placeholder');
+                    }}>
+                        {#if contact.avatar !== null}
+                            <img class="avatar_mini" src={contact.avatar} alt="d" />
+                        {:else}
+                            <img class="avatar_mini flex flex-nowrap" src="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-512.png" alt="" />
+                        {/if}
+                        <div class="font-bold text-xl m-5" on:click>
+                            {contact.firstname} {contact.lastname}
+                        </div>
+                        <div class="object-right text-sm">
+                            {contact.phone}
+                        </div>
+                    </div>
+                {/each}
+            {:catch error}
+                <p>Error</p>
+                <pre>{error}</pre>
+            {/await}
+        </div>
+    </div>
+</div>
